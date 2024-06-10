c0f99fbfe07f4fea634c10db34b79e06acb2bf5e
HoshinoSuzumi <master@uniiem.com>
2024-06-10 11:52:04 +0800

    wip

--- app/[locale]/providers.tsx
+++ app/[locale]/providers.tsx
@@ -3,14 +3,17 @@
 import { ReactNode } from 'react'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { useRouter } from '@/navigation'
 
 export function Providers({
   children,
 }: {
   children: ReactNode
 }) {
+  const router = useRouter()
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={ router.push }>
       <NextThemesProvider attribute="class">
         { children }
       </NextThemesProvider>
