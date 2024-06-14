efb981480a6a5c59bf76ee2fe4844f5154fb7fc3
HoshinoSuzumi <master@uniiem.com>
2024-06-14 23:24:13 +0800

    feat: add favicon and gfm support

--- lib/md2html.ts
+++ lib/md2html.ts
@@ -1,7 +1,13 @@
 import { remark } from 'remark'
-import html from 'remark-html'
+import remarkHtml from 'remark-html'
+import remarkToc from 'remark-toc'
+import remarkGfm from 'remark-gfm'
 
 export default async function md2html(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark()
+  .use(remarkHtml)
+  .use(remarkGfm)
+  .use(remarkToc, { ordered: true })
+  .process(markdown)
   return result.toString()
 }
