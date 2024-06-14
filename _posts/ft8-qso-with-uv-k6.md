c06d0a728201b56ab0723c24a11fdceb5e1118b6
HoshinoSuzumi <master@uniiem.com>
2024-06-14 12:58:35 +0800

    post: ft8-qso-with-uv-k6 todo

--- _posts/ft8-qso-with-uv-k6.md
+++ _posts/ft8-qso-with-uv-k6.md
@@ -1,6 +1,35 @@
 ---
-title: "尝试使用 UV-K6 进行 FT8 通联"
-date: "2024-06-13T20:18:07.322Z"
+title: "UV-K6 基于 CEC 固件进行 FT8 通联"
+date: "2024-06-14T10:18:00.000Z"
 ---
 
-To be written...
+## 什么是 FT8
+
+FT8 (Franke-Taylor 设计的 8-FSK 调制) 适用于业余无线电通信的一种数字模式，由 K9AN (Steve Franke) 和 K1JT (Joe Taylor) 在
+2017 年发布。得益于 FT8 的高效 (50Hz 窄带) 和低信噪比 (低至 -24dB)，适合在低功率和恶劣传播条件下的通信。FT8
+包含一组固定的消息格式，可以用于交换呼号、信号报告和 QTH 等信息。与 FT8 一同发布的软件 WSJT (
+后来发展成为开源软件 [WSJT-X](https://wsjt.sourceforge.io/index.html))
+可以自动应答和呼叫，实现自动挡通联。
+
+## 固件选择
+
+[CEC](https://github.com/phdlee/uvk5cec) 是众多 UV-K6
+开源固件中的一个，在最近更新的 [0.3Q](https://github.com/phdlee/uvk5cec/releases/tag/v0.3q) 版本开始，支持了 FT8 模式以及与
+WSJT-X 的数字通信。
+
+![CEC 0.3V](/assets/posts/ft8-qso-with-uv-k6/img.png)
+
+截至这篇文章编写之日，CEC 最新的版本为 0.3V，该版本新增了多数常用的 FT8 频率；更新了 UV-K5 DigiManager，可以直接发送 FT8
+测试数据。下载上图中的 **0.3v.zip**、**UVK5DigManager_v1.0.zip**、**wsjtx-2.6.1-win64_forUVK5.exe**
+
+关于如何刷入固件，请参考其他教程。推荐使用 BD8DFN 开发的 [K5Web](https://mm.md/#/tool/flash) 工具在线刷入
+
+## 线缆制作
+
+我们需要改造或制作一根专用线缆，以实现 WSJT-X 与 UV-K6 之间的数字通信和数据收发。
+
+- 泉盛 UV-K6
+- Kenwood 连接头 (K头)
+- 3.5mm 4段音频线
+
+> To be written...
--- app/[locale]/ActionBar.tsx
+++ app/[locale]/ActionBar.tsx
@@ -3,10 +3,11 @@
 import './ActionBar.scss'
 import TablerBuildingBroadcastTower from '@/components/Icons/TablerBuildingBroadcastTower'
 import { noto_sc, saira } from '@/app/[locale]/fonts'
-import { Link } from '@/navigation'
+import { Link, usePathname } from '@/navigation'
 import { LanguageChanger } from '@/components/LanguageChanger'
 import { SignIn } from '@/components/auth-components'
 import { useTranslations } from 'next-intl'
+import { useEffect, useState } from 'react'
 
 export const ActionBar = ({
   locale,
@@ -14,23 +15,42 @@ export const ActionBar = ({
   locale: string
 }) => {
   const t = useTranslations('home')
+  const pathname = usePathname()
+  const [headerExtraClass, setHeaderExtraClass] = useState('')
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY
+      if (pathname.startsWith('/posts') && scrollY > 0) {
+        setHeaderExtraClass('-translate-y-1/2 opacity-0 pointer-events-none')
+      } else {
+        setHeaderExtraClass('')
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [pathname])
+
   return (
-    <header className={ `action-bar ${ noto_sc.className }` }>
-      <Link className={ 'flex items-center gap-2 select-none' } href={ '/' }>
-        <TablerBuildingBroadcastTower className={ 'text-2xl' }/>
-        <h1 className={ `flex flex-col leading-tight text-xl font-medium ${ saira.className }` }>
+    <header className={ `action-bar ${ noto_sc.className } ${ headerExtraClass } transition` }>
+      <div className={ 'flex justify-between items-center container xl:max-w-[1280px]' }>
+        <Link className={ 'flex items-center gap-2 select-none' } href={ '/' }>
+          <TablerBuildingBroadcastTower className={ 'text-2xl' }/>
+          <h1 className={ `flex flex-col leading-tight text-xl font-medium ${ saira.className }` }>
           <span>
             <span className={ 'font-bold' }>BH8GA</span>
             <span className={ 'opacity-50' }>&apos;s values</span>
           </span>
-          <span className={ 'text-xs' }>
+            <span className={ 'text-xs' }>
             { t('my_internet_oasis') }
           </span>
-        </h1>
-      </Link>
-      <div className={ 'flex items-center gap-2' }>
-        <SignIn/>
-        <LanguageChanger locale={ locale }/>
+          </h1>
+        </Link>
+        <div className={ 'flex items-center gap-2' }>
+          <SignIn/>
+          <LanguageChanger locale={ locale }/>
+        </div>
       </div>
     </header>
   )
