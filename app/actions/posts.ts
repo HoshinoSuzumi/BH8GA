25a4320fa064a6a337395f51b0f570c7f2a85a5e
HoshinoSuzumi <master@uniiem.com>
2024-10-26 10:34:42 +0800

    🐛 Fix posts sort

--- app/actions/posts.ts
+++ app/actions/posts.ts
@@ -37,5 +37,5 @@ export function getAllPosts(): Post[] {
   const slugs = getPostSlugs()
   return slugs
   .map((slug) => getPostBySlug(slug)!)
-  .sort((post1, post2) => (post1!.date > post2!.date ? -1 : 1))
+  .sort((post1, post2) => (post1!.date > post2!.date ? 1 : -1))
 }
